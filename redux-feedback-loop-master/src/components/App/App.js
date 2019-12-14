0be5486e9edd4ea9a66c9faf7e65a852c4671718
Josh Ganote <j.ganote0825@gmail.com>
2019-12-13 19:14:57 -0600

    setup redux to begin using dispatch calls

--- redux-feedback-loop-master/src/components/App/App.js
+++ redux-feedback-loop-master/src/components/App/App.js
@@ -40,12 +40,14 @@ class App extends Component {
 
   render() {
     return (
+     
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
         <br/>
+        
       </div>
     );
   }
---
+++ redux-feedback-loop-master/src/components/redux/mapStoreToProps.js
@@ -0,0 +1,8 @@
+const mapStoreToProps = (store) => {
+    return {
+        store: store,
+    }
+}
+
+
+export default mapStoreToProps;
